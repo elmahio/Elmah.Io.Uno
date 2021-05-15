6f6221b46fadd325a9af4a3eb13f2ad41d22518d
Thomas Ardal <thomasardal@gmail.com>
2021-05-15 12:47:47 +0200

    Upgraded unoplatform

--- samples/Elmah.Io.UnoSample/Elmah.Io.UnoSample.Shared/MainPage.xaml.cs
+++ samples/Elmah.Io.UnoSample/Elmah.Io.UnoSample.Shared/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Uno.Extensions;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -13,7 +14,6 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using Uno.Extensions;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -32,7 +32,7 @@ namespace Elmah.Io.UnoSample
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             // Manually log an error to elmah.io
-            this.Log().LogError(1, new ApplicationException(), "Hello World from Uno");
+            this.Log().LogError(new ApplicationException(), "Hello World from Uno");
 
             // Throwing an exception will also automatically log an error
             throw new ApplicationException("Oh no someone clicked the button");
---
+++ src/Elmah.Io.Uno/ElmahIoLoggingBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Elmah.Io.Uno;
+using Microsoft.Extensions.Logging;
+
+namespace Elmah.Io.Uno
+{
+    public static class ElmahIoLoggingBuilderExtensions
+    {
+        public static ILoggingBuilder AddElmahIo(this ILoggingBuilder builder, string apiKey, Guid logId)
+        {
+            builder.AddProvider(new ElmahIoLoggerProvider(apiKey, logId));
+            return builder;
+        }
+
+        //public static ILoggingBuilder AddElmahIo(this ILoggingBuilder builder)
+        //{
+        //    builder.Services.AddSingleton<ILoggerProvider, ElmahIoLoggerProvider>(services =>
+        //    {
+        //        var options = services.GetService<IOptions<ElmahIoProviderOptions>>();
+        //        return new ElmahIoLoggerProvider(options);
+        //    });
+        //    return builder;
+        //}
+
+    }
+}
